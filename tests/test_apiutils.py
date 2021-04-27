f3c744067c8f05a57884f018594feb228c3575ee
renae-r <rodge103@umn.edu>
2021-04-26 20:28:17 -0500

    tests running

--- tests/test_apiutils.py
+++ tests/test_apiutils.py
@@ -6,51 +6,50 @@ from ipumspy import apiutils
 
 @pytest.fixture(params=[os.getenv('MY_KEY')])
 def api_util(request):
-    return apiutils.ApiUtilities(request.param)
+    return apiutils.IpumsApiClient(request.param)
 
 def test_build_extract(api_util):
     '''
-    Confirm that text extract formatted correctly
+    Confirm that test extract formatted correctly
     '''
-    #api_util = apiutils.ApiUtilities(api_key)
-    extract = api_util.extract_request
-    extract.build('cps',
-                 ['cps1976_01s', 'cps1976_02b'],
-                 ['YEAR', 'MISH', 'AGE',
-                 'RACE', 'UH_SEX_B1'])
-    extract_def = extract.extract_definition
-    assert extract_def == {
-                            'data_structure': { 
-                                'rectangular': {
-                                    'on': 'P'
-                                }
-                            },
-                            'samples': {
-                            'cps1976_01s': {},
-                            'cps1976_02b': {}
-                            },
-                            'variables':{
-                            'YEAR': {},
-                            'MISH': {},
-                            'AGE': {},
-                            'RACE': {},
-                            'UH_SEX_B1': {}
-                            },
-                            'description': 'My IPUMS extract',
-                            'data_format': 'fixed_width',
+
+    extract = api_util.build_extract('cps',
+                                    ['cps1976_01s', 'cps1976_02b'],
+                                    ['YEAR', 'MISH', 'AGE',
+                                    'RACE', 'UH_SEX_B1'])
+    assert extract == {
+                        'data_structure': { 
+                            'rectangular': {
+                                'on': 'P'
+                            }
+                        },
+                        'samples': {
+                        'cps1976_01s': {},
+                        'cps1976_02b': {}
+                        },
+                        'variables':{
+                        'YEAR': {},
+                        'MISH': {},
+                        'AGE': {},
+                        'RACE': {},
+                        'UH_SEX_B1': {}
+                        },
+                        'description': 'My IPUMS extract',
+                        'data_format': 'fixed_width',
                         }
-    assert extract.product == 'cps'
-    assert extract.status == 'built'
     
 def test_submit_extract(api_util):
-    api_util.extract_request.build('cps',
-                                   ['cps1976_01s', 'cps1976_02b'],
-                                   ['YEAR', 'MISH', 'AGE',
-                                    'RACE', 'UH_SEX_B1'])
-    extract = api_util.extract_request.submit()
+    '''
+    Confirm that test extract submits properly
+    '''
+    extract_def = api_util.build_extract('cps',
+                                        ['cps1976_01s', 'cps1976_02b'],
+                                        ['YEAR', 'MISH', 'AGE',
+                                         'RACE', 'UH_SEX_B1'])
+    extract, number = api_util.submit_extract('cps', extract_def)
     assert extract.status_code == 200
 
 def test_retrieve_previous_extracts(api_util):
-    previous = api_util.extract_history.retrieve_previous_extracts('cps')
+    previous = api_util.retrieve_previous_extracts('cps')
     assert len(previous) == 10
 
