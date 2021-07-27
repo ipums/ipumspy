c3a900bfee52e2b993d29ae0ca1bfa6350f386f8
renae-r <rodge103@umn.edu>
2021-07-27 10:10:56 -0500

    add some tests for exceptions

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,7 @@ import time
 import pytest
 
 from ipumspy.api import IpumsApiClient, OtherExtract, UsaExtract
+from ipumspy.api.exceptions import BadIpumsApiRequest
 
 
 @pytest.fixture(scope="module")
@@ -25,6 +26,12 @@ def api_client(environment_variables, mock_api: str) -> IpumsApiClient:
     client = IpumsApiClient(os.environ.get("IPUMS_API_KEY"))
     client.base_url = mock_api
     return client
+
+
+@pytest.fixture(scope="function")
+def live_api_client(environment_variables) -> IpumsApiClient:
+    live_client = IpumsApiClient(os.environ.get("IPUMS_API_KEY"))
+    return live_client
 
 
 def test_usa_build_extract():
@@ -67,3 +74,32 @@ def test_retrieve_previous_extracts(api_client: IpumsApiClient):
     previous = api_client.retrieve_previous_extracts()
     assert "usa" in previous
     assert len(previous["usa"]) == 10
+
+
+def test_bad_api_request_exception(live_api_client: IpumsApiClient):
+    """
+    Confirm that malformed or impossible extract requests raise
+    BadIpumsApiRequest exception
+    """
+    # bad variable
+    bad_variable = UsaExtract(["us2012b"], ["AG"])
+    with pytest.raises(BadIpumsApiRequest) as exc_info:
+        live_api_client.submit_extract(bad_variable)
+    assert exc_info.type is BadIpumsApiRequest
+    assert exc_info.value.args[0] == "Invalid variable name: AG"
+
+    # unavailable variable
+    unavailable_variable = UsaExtract(["us2012b"], ["YRIMMIG"])
+    with pytest.raises(BadIpumsApiRequest) as exc_info:
+        live_api_client.submit_extract(unavailable_variable)
+    assert exc_info.type is BadIpumsApiRequest
+    assert exc_info.value.args[0] == ("YRIMMIG: This variable is not available in any "
+                                      "of the samples currently selected. Click the "
+                                      "variable name to see its sample availability.")
+
+    # bad sample
+    bad_sample = UsaExtract(["us2012x"], ["AGE"])
+    with pytest.raises(BadIpumsApiRequest) as exc_info:
+        live_api_client.submit_extract(bad_sample)
+    assert exc_info.type is BadIpumsApiRequest
+    assert exc_info.value.args[0] == "Invalid sample name: us2012x"
