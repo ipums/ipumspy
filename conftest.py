cf5235189cd0f34ec6ce5416cdbcc0a398b17113
renae-r <rodge103@umn.edu>
2021-07-29 09:45:37 -0500

    typo fix

--- conftest.py
+++ conftest.py
@@ -20,7 +20,7 @@ def pytest_addoption(parser):
 
 def pytest_collection_modifyitems(config, items):
     skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    skip_int = pytest.mark.skip(reason="need --runing option to run")
+    skip_int = pytest.mark.skip(reason="need --runint option to run")
     for item in items:
         if "slow" in item.keywords and not config.getoption("--runslow"):
             item.add_marker(skip_slow)
