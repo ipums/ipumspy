db93f1fc96264497011dd9cc8bf7dfa3ef2fc2e3
renae-r <rodge103@umn.edu>
2021-11-18 18:31:55 -0600

    add test for var with codes

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -22,7 +22,18 @@ def test_tabulate(fixtures_path: Path):
     year_info = ddi.get_variable_info("YEAR")
     crosstab_df = utilities.tabulate(year_info, data)
 
-    assert list(crosstab_df.columns) == ['counts', 'pct']
+    assert list(crosstab_df.columns) == ["counts", "pct"]
     assert (crosstab_df["counts"]).all() == (np.array([4065, 3603])).all()
     assert (crosstab_df["pct"]).all() == (np.array([0.530125, 0.469875])).all()
+
+    ddi = readers.read_ipums_ddi(fixtures_path / "cps_00006.xml")
+    data = readers.read_microdata(ddi, fixtures_path / "cps_00006.dat.gz")
+    month_info = ddi.get_variable_info("MONTH")
+    crosstab_df = utilities.tabulate(month_info, data)
+
+    assert list(crosstab_df.columns) == ["val", "lab", "counts", "pct"]
+    assert (crosstab_df["val"]).all() == (np.array([3])).all()
+    assert list(crosstab_df["lab"]) == ["March"]
+    assert (crosstab_df["counts"]).all() == (np.array([7668])).all()
+    assert (crosstab_df["pct"]).all() == (np.array([1.0])).all()
 
