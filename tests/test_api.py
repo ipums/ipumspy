806d08a1e9d8d87aadfd3e729e6e4af98719a55e
renae-r <rodge103@umn.edu>
2022-02-17 23:15:53 -0600

    add tests for extract to and from dict methods

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,15 @@
 import os
 import subprocess
 import time
+import yaml
+import json
+import pickle
+from pathlib import Path
 
 import pytest
 
 from ipumspy import api
-from ipumspy.api import IpumsApiClient, OtherExtract, UsaExtract
+from ipumspy.api import IpumsApiClient, OtherExtract, UsaExtract, extract_from_dict, extract_to_dict
 from ipumspy.api.exceptions import BadIpumsApiRequest, IpumsApiException, IpumsNotFound
 
 
@@ -159,3 +163,44 @@ def test_extract_was_purged(live_api_client: IpumsApiClient):
     """
     was_purged = live_api_client.extract_was_purged(extract="1", collection="usa")
     assert was_purged == True
+
+
+def test_extract_from_dict(fixtures_path: Path):
+    with open(str(fixtures_path / "example_extract.yml")) as infile:
+        extract = extract_from_dict(yaml.safe_load(infile))
+        infile.close()
+        for item in extract:
+            assert item.collection == "usa"
+            assert item.samples == ["us2012b"]
+            assert item.variables == ["AGE", "SEX", "RACE"]
+
+    with open(str(fixtures_path / "example_extract.json")) as infile:
+        extract = extract_from_dict(json.load(infile))
+        infile.close()
+        for item in extract:
+            assert item.collection == "usa"
+            assert item.samples == ["us2012b"]
+            assert item.variables == ["AGE", "SEX", "RACE"] 
+
+
+def test_extract_to_dict(fixtures_path: Path):
+    # reconstitute the extract object from pickle
+    with open(str(fixtures_path / "usa_extract_obj.pkl"), "rb") as infile:
+        extract = pickle.load(infile)
+        infile.close()
+
+    # export extract to dict
+    dct = extract_to_dict(extract)
+    assert dct["collection"] == "usa"
+    assert dct["samples"] == {"us2012b": {}}
+    assert dct["variables"] == {'YEAR': {'preselected': True},
+                                'SAMPLE': {'preselected': True},
+                                'SERIAL': {'preselected': True},
+                                'CBSERIAL': {'preselected': True},
+                                'GQ': {'preselected': True},
+                                'HHWT': {'preselected': True},
+                                'PERNUM': {'preselected': True},
+                                'PERWT': {'preselected': True},
+                                "AGE": {}, 
+                                "SEX": {}, 
+                                "RACE": {}}
